1957d13320c93121da83d875aeef0bc1840bee33
Mhycka1 <mmcdon10@terpmail.umd.edu>
2023-11-09 20:05:08 -0500

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // State to store the randomly generated number and letter
+  const [randomNumber, setRandomNumber] = useState(null);
+  const [randomLetter, setRandomLetter] = useState(null);
+
+  // Function to handle the button click and generate a random number and letter
+  const handleButtonClick = () => {
+    // Generate a random number between 1 and 10
+    const newRandomNumber = Math.floor(Math.random() * 10) + 1;
+    // Generate a random letter
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const newRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+    // Update the state with the new random number and letter
+    setRandomNumber(newRandomNumber);
+    setRandomLetter(newRandomLetter);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* Display the randomly generated number and letter */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Random Number: {randomNumber}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Random Letter: {randomLetter}
+        </p>
+        <button className="App-button" onClick={handleButtonClick}>
+          Generate Random Number and Letter
+        </button>
       </header>
     </div>
   );
